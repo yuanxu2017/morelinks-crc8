5ae8b09d919adfdee211873b07a8a04f2659e893
21334878@qq.com <21334878@qq.com>
2017-10-16 19:50:40 +0800

    update

--- crc8.js
+++ crc8.js
@@ -1,22 +1,24 @@
 'use strict';
 /**
- * Calculates the buffers CRC16.
+ * Calculates the buffers CRC8.
  *
  * @param {Buffer} buffer the data buffer.
- * @return {number} the calculated CRC16.
+ * @return {number} the calculated CRC8.
  */
-module.exports = function crc16(buffer) {
-    var crc = 0xFFFF;
+module.exports = function crc8(buffer) {
+    var crc = 0;
     var odd;
 
     for (var i = 0; i < buffer.length; i++) {
         crc = crc ^ buffer[i];
 
         for (var j = 0; j < 8; j++) {
-            odd = crc & 0x0001;
-            crc = crc >> 1;
+            odd = crc & 0x80;
+            crc = crc << 1;
             if (odd) {
-                crc = crc ^ 0xA001;
+                crc = crc ^ 0x07%256;
+            }else {
+                crc=crc%256;
             }
         }
     }
